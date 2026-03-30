074d3e6993df19f955895711ff2dfd675b6c846f
peter_baserow <peter@baserow.io>
2026-03-30 17:21:01 +0100

    Fix migration conflict

--- backend/src/baserow/contrib/automation/migrations/0027_alter_automationworkflow_graph.py
+++ backend/src/baserow/contrib/automation/migrations/0027_alter_automationworkflow_graph.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('automation', '0025_automationnoderesult_iteration_path'),
+        ('automation', '0026_alter_automationworkflow_unique_together'),
     ]
 
     operations = [
